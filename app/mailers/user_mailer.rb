88094be246798225c5ddc7e73eb014efa1ca6fd2
auriane gregoire <auriane.gregoire@gmail.com>
2021-06-05 12:23:48 +0200

    emails set up & content

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,21 @@
 class UserMailer < ApplicationMailer
   default from: 'auriane.gregoire@gmail.com'
 
+  # Email received right after submiting the form to get on the waiting list
   def welcome_email
     @user = params[:user]
     mail(to: @user.email, subject: 'Confirm you inscription to Brest waiting list')
+  end
+
+  # Email received every 15 days
+  def confirmation_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Warning: still looking for a coworking place?')
+  end
+
+  # Email received if the last validation was 20 days ago
+  def cancel_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'You\'re no longer on the waiting list for Brest Coworking')
   end
 end
