e77432c51982946d6aeb6bf3f1778c50dc83f467
auriane gregoire <auriane.gregoire@gmail.com>
2021-06-05 22:04:06 +0200

    sidekiq v3

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,15 @@
+redis_conn = proc {
+  Redis.new # do anything you want here
+}
+
+Sidekiq.configure_client do |config|
+  config.redis = ConnectionPool.new(size: 5, &redis_conn)
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = ConnectionPool.new(size: 25, &redis_conn)
+end
+
 schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
