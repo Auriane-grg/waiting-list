0550b8b4a6056885384e55d1c53b57f4c0d2ba80
auriane gregoire <auriane.gregoire@gmail.com>
2021-06-05 12:31:45 +0200

    test preview mailer

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,15 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+  def welcome_email
+    UserMailer.with(user: WaitingUser.first).welcome_email
+  end
+
+  def confirmation_email
+    UserMailer.with(user: WaitingUser.first).confirmation_email
+  end
+
+  def cancelled_email
+    UserMailer.with(user: WaitingUser.first).cancelled_email
+  end
 end
